ca077786cb2bd3c57a395dab061ccfe55844edca
pierknows1 <128331075+pierknows1@users.noreply.github.com>
2023-06-28 14:03:21 -0400

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // new line
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {                 // new line
+      contentBase: './dist'      // new line
+    },                           // new line
+    devtool: 'eval-source-map',  
+  plugins: [
+    new CleanWebpackPlugin({
+        verbose: true
+      }),
+    new HtmlWebpackPlugin({
+      title: 'Shape Tracker',
+      template: './src/index.html',
+      inject: 'body'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  }
+};
